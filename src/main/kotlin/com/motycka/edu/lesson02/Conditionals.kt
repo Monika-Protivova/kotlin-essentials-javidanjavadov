16426ef37daaf47995455e0207ccbeeb4c96e7b1
javidanjavadov <javidan.javadov@gmail.com>
2025-06-23 19:42:40 +0400

    Lesson 02 Task 3: Implement conditionals using if-else and when expressions

--- src/main/kotlin/com/motycka/edu/lesson02/Conditionals.kt
+++ src/main/kotlin/com/motycka/edu/lesson02/Conditionals.kt
@@ -1,6 +1,5 @@
 package com.motycka.edu.lesson02
 
-
 const val ESPRESSO_PRICE = 2.5
 const val DOUBLE_ESPRESSO_PRICE = 3.0
 const val CAPPUCCINO_PRICE = 3.0
@@ -8,13 +7,6 @@ const val LATTE_PRICE = 3.5
 const val AMERICANO_PRICE = 2.0
 const val FLAT_WHITE_PRICE = 3.2
 
-/*
-    1. Get price bas on item names
-    2. Get the lowest price from a list of prices
-    3. Calculate a total price for a list of items
-    4. Calculate a discount if 3 or more items are ordered (the cheapest item is free)
-    5. Print the total price with a discount applied for each order
- */
 fun conditionals() {
     val coffeeOrders = mapOf(
         1 to listOf(ESPRESSO, CAPPUCCINO, LATTE, AMERICANO),
@@ -22,11 +14,31 @@ fun conditionals() {
     )
 
     coffeeOrders.forEach { orderId, items ->
-
         println("Processing Order ID: $orderId")
         println("Items: $items")
 
-        val total: Double = TODO("Calculate total price for the order")
+        val prices = items.map { item ->
+            when (item) {
+                ESPRESSO -> ESPRESSO_PRICE
+                DOUBLE_ESPRESSO -> DOUBLE_ESPRESSO_PRICE
+                CAPPUCCINO -> CAPPUCCINO_PRICE
+                LATTE -> LATTE_PRICE
+                AMERICANO -> AMERICANO_PRICE
+                FLAT_WHITE -> FLAT_WHITE_PRICE
+                else -> 0.0
+            }
+        }
+
+        val totalWithoutDiscount = prices.sum()
+
+        val discount = if (items.size >= 3) {
+            println("You ordered 3 or more coffees, you get 1 for free!")
+            prices.minOrNull() ?: 0.0
+        } else {
+            0.0
+        }
+
+        val total = totalWithoutDiscount - discount
 
         println("Total price for Order ID $orderId: $total\n")
     }
