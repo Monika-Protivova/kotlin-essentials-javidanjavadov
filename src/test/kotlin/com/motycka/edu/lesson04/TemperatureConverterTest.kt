df0bb1147eb672b79ef1f4e74b925442690445f6
javidanjavadov <javidan.javadov@gmail.com>
2025-06-23 19:42:42 +0400

    Lesson 04 Task 1: Add unit tests for temperature conversion functions using Kotest

--- src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
+++ src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
@@ -1,11 +1,31 @@
 package com.motycka.edu.lesson04
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
 
-// By convention, the name of the test class should be the name of the class under test + "Test"
 class TemperatureConverterTest : StringSpec({
+    "convert Fahrenheit 32 to Celsius should be 0" {
+        val fahrenheit = 32.0
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+        celsius shouldBe 0.0
+    }
 
-    // TODO Implement tests for the TemperatureConverter class
+    "convert Fahrenheit 212 to Celsius should be 100" {
+        val fahrenheit = 212.0
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+        celsius shouldBe 100.0
+    }
 
+    "convert Fahrenheit 98.6 to Celsius should be approx 37" {
+        val fahrenheit = 98.6
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+        celsius shouldBe (37.0 plusOrMinus 0.1)
+    }
+
+    "convert negative Fahrenheit to Celsius" {
+        val fahrenheit = -40.0
+        val celsius = TemperatureConverter.toCelsius(fahrenheit)
+        celsius shouldBe -40.0
+    }
 })
